542f1ef2acf7550b2f193468b143247fe6ac537a
Lori Culberson <bigskybandit@sbcglobal.net>
2015-02-04 22:05:41 -0700

    Guess checker works with a guess counter

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,8 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret_num ||= rand(100)
+guess_count = 0
 
 get '/' do 
-  erb :index, :locals => {:secret_num => secret_num }
+  message = ""
+  guess = params['guess'].to_i
+  guess_count += 1
+  
+  message =  guess_check(secret_num, guess) if guess_count!= 1
+  erb :index, :locals => {:secret_num => secret_num, :message => message, :guess_count => guess_count}
 
 end
+
+def guess_check(secret_num, guess)
+  case 
+    when secret_num < guess then message = "Too high" 
+    when secret_num > guess then message = "Too low"
+    else "You are correct!"
+  end
+end
+
