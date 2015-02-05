7cf9d9a17c9e591e894a541c2817487cc0ea4065
Lori Culberson <bigskybandit@sbcglobal.net>
2015-02-04 23:04:54 -0700

    I3:number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -9,16 +9,20 @@ get '/' do
   guess = params['guess'].to_i
   guess_count += 1
   
-  message =  guess_check(secret_num, guess) if guess_count!= 1
+  message =  guess_check(secret_num, guess, guess_count) if guess_count!= 1
   erb :index, :locals => {:secret_num => secret_num, :message => message, :guess_count => guess_count}
 
 end
 
-def guess_check(secret_num, guess)
+def guess_check(secret_num, guess, guess_count)
   case 
-    when secret_num < guess then message = "Too high" 
-    when secret_num > guess then message = "Too low"
-    else "You are correct!"
+    when guess - secret_num > 5 then "Way too high!"
+    when secret_num < guess then "Too high" 
+    when secret_num - guess > 5 then "Way too low"
+    when secret_num > guess then "Too low"
+    when secret_num == guess && guess_count == 2 then "You are correct! And is took you #{guess_count-1} guess!"
+    else
+      "You are correct! And is took you #{guess_count-1} guesses!"
   end
 end
 
