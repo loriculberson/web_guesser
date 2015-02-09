b803b84dc580620e8c822af83497f38e69633aba
Lori Culberson <bigskybandit@sbcglobal.net>
2015-02-08 17:36:36 -0700

    background colors change with guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -18,7 +18,7 @@ def guess_check(secret_num, guess, guess_count)
   case 
     when guess - secret_num > 5 then "Way too high!"
     when secret_num < guess then "Too high" 
-    when secret_num - guess > 5 then "Way too low"
+    when secret_num - guess > 5 then "Way too low!"
     when secret_num > guess then "Too low"
     when secret_num == guess && guess_count == 2 then "You are correct! And is took you #{guess_count-1} guess!"
     else
